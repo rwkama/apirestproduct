605982d37ddf0eb32bdb5feee7823a0c0293398f
Carlos Andres Rodriguez <rwkama27@gmail.com>
2020-10-06 16:16:43 -0300

    another commit

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(__dirname + '/images'));
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/images', express.static(__dirname + '/images'));
 app.use('/', routes);
 app.use('/users', users);
 
